3aa1d95678969230edeadf170d4bca5d7c521d0b
noorjahan220 <noorjahanmeem220@gmail.com>
2025-07-01 22:27:22 +0600

    Update CORS settings

--- index.js
+++ index.js
@@ -6,22 +6,27 @@ require('dotenv').config(); const bcrypt = require('bcrypt');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const allowedOrigins = [
-  "https://event-sphere-6vrsuh7hl-noorjahan-akters-projects.vercel.app",
-  "https://event-sphere-77mpyx2oy-noorjahan-akters-projects.vercel.app",
-  "http://localhost:5173"
-];
 
-app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin) return callback(null, true); // allow Postman or server-to-server
-    if (allowedOrigins.indexOf(origin) === -1) {
-      return callback(new Error("Not allowed by CORS"), false);
-    }
-    return callback(null, origin); // reflect the origin dynamically
-  },
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: [
+      "https://event-sphere-6vrsuh7hl-noorjahan-akters-projects.vercel.app",
+      "https://event-sphere-77mpyx2oy-noorjahan-akters-projects.vercel.app",
+      "http://localhost:5173"
+    ],
+    credentials: true,
+  })
+);
+// app.use(cors({
+//   origin: function(origin, callback) {
+//     if (!origin) return callback(null, true); // allow Postman or server-to-server
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       return callback(new Error("Not allowed by CORS"), false);
+//     }
+//     return callback(null, origin); // reflect the origin dynamically
+//   },
+//   credentials: true,
+// }));
 app.use(express.json());
 
 
@@ -39,7 +44,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
 
 
